8e8ace58d391202b53f91468e4b0a4ceaa5b55ef
Milad <milad.shahabi.20@gmail.com>
2023-05-29 14:35:57 +0200

    first step

--- telegram_bot_module.py
+++ telegram_bot_module.py
@@ -1,6 +1,6 @@
 from typing import Final
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, CallbackQueryHandler
 
 TOKEN: Final = '6114910026:AAGGPogroG1BvkHA3LOTab_0EzBqqi3JQYM'
 BOT_USERNAME: Final = '@GetMyTermin_bot'
@@ -74,3 +74,78 @@ if __name__ == '__main__':
     #Polls the bot
     print('Polling...')
     app.run_polling(poll_interval=3)
+
+
+
+
+# import logging
+
+# from telegram import __version__ as TG_VER
+
+# try:
+#     from telegram import __version_info__
+# except ImportError:
+#     __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
+
+# if __version_info__ < (20, 0, 0, "alpha", 1):
+#     raise RuntimeError(
+#         f"This example is not compatible with your current PTB version {TG_VER}. To view the "
+#         f"{TG_VER} version of this example, "
+#         f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
+#     )
+# from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+# from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes
+
+# # Enable logging
+# logging.basicConfig(
+#     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+# )
+# logger = logging.getLogger(__name__)
+
+
+# async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     """Sends a message with three inline buttons attached."""
+#     keyboard = [
+#         [
+#             InlineKeyboardButton("Option 1", callback_data="1"),
+#             InlineKeyboardButton("Option 2", callback_data="2"),
+#         ],
+#         [InlineKeyboardButton("Option 3", callback_data="3")],
+#     ]
+
+#     reply_markup = InlineKeyboardMarkup(keyboard)
+
+#     await update.message.reply_text("Please choose:", reply_markup=reply_markup)
+
+
+# async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     """Parses the CallbackQuery and updates the message text."""
+#     query = update.callback_query
+
+#     # CallbackQueries need to be answered, even if no notification to the user is needed
+#     # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+#     await query.answer()
+
+#     await query.edit_message_text(text=f"Selected option: {query.data}")
+
+
+# async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     """Displays info on how to use the bot."""
+#     await update.message.reply_text("Use /start to test this bot.")
+
+
+# def main() -> None:
+#     """Run the bot."""
+#     # Create the Application and pass it your bot's token.
+#     application = Application.builder().token("TOKEN").build()
+
+#     application.add_handler(CommandHandler("start", start))
+#     application.add_handler(CallbackQueryHandler(button))
+#     application.add_handler(CommandHandler("help", help_command))
+
+#     # Run the bot until the user presses Ctrl-C
+#     application.run_polling()
+
+
+# if __name__ == "__main__":
+#     main()
